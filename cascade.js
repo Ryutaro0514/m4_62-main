f6b3ebd667e06ef7ec08c3833f59a9b261c3046f
Ryutaro0514 <ryuryu_758_3602@icloud.com>
2025-10-19 02:08:42 +0900

    完成

--- cascade.js
+++ cascade.js
@@ -1,1 +1,30 @@
-const right
+const right = document.querySelector(".right");
+const left = document.querySelector(".left");
+const img = document.querySelectorAll(".img-all img, .img-all video");
+let fripIndex = 0;
+
+function frip(index) {
+  img.forEach((item, i) => {
+    item.style.display = i === index ? "block" : "none";
+    if (item.tagName.toLowerCase() === "video") {
+      if (i === index) {
+        item.currentTime = 0;
+        item.play();
+      } else {
+        item.pause();
+      }
+    }
+  });
+  fripIndex = index;
+}
+
+left.addEventListener("click", () => {
+  fripIndex = (fripIndex - 1 + img.length) % img.length;
+  frip(fripIndex);
+});
+right.addEventListener("click", () => {
+  fripIndex = (fripIndex + 1) % img.length;
+  frip(fripIndex);
+});
+
+frip(0);
